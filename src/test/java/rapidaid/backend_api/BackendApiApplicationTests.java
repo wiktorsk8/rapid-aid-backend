df657a792381df73fcc46e320a4abd94a4a05145
wiktorsk8 <kowalinski.wiktor04@gmail.com>
2024-10-12 12:26:07 +0200

    init

---
+++ src/test/java/rapidaid/backend_api/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rapidaid.backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
